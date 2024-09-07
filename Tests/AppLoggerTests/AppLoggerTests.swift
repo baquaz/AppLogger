d7313e2f86342b267285f66e7383bb8d51077092
pb <9070569+baquaz@users.noreply.github.com>
2024-09-07 18:04:39 +0200

    Initial Commit

---
+++ Tests/AppLoggerTests/AppLoggerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import AppLogger
+
+final class AppLoggerTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}

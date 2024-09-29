6d22d424dfb45a2b046d42783ff8d0a7d3bee1b2
Tomasz Popi <tomek@melon-it.com>
2024-09-29 15:41:22 +0200

    code cleaning

--- Sources/AppLogger/LogStrategy.swift
+++ Sources/AppLogger/LogStrategy.swift
@@ -21,25 +21,21 @@ public struct DefaultLogStrategy: LogStrategy {
   public var defaultLogType: LogType = DefaultLogType.debug
   
   public func log(message: String, tag: any LogType, category: String) {
-    let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "--", category: category)
+    
+    let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "--",
+                        category: category)
+    
     switch tag {
       case let tag as DefaultLogType:
         switch tag {
-          case .error:
-            logger.fault("\(message)")
-          case .warning:
-            logger.warning("\(message)")
-          case .success:
-            logger.info("\(message)")
-          case .debug:
-            logger.debug("\(message)")
-          case .network:
-            logger.info("\(message)")
-          case .simOnly:
-            logger.info("\(message)")
+          case .error: logger.fault("\(message)")
+          case .warning: logger.warning("\(message)")
+          case .success: logger.info("\(message)")
+          case .debug: logger.debug("\(message)")
+          case .network: logger.info("\(message)")
+          case .simOnly: logger.info("\(message)")
         }
-      default:
-        logger.log("\(message)")
+      default: logger.log("\(message)")
     }
   }
 }
